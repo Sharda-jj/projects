33cd875292bea3fcbfbd4805b3da438a77607ad3
Sharda-jj <shardajadhav126@gmail.com>
2025-03-26 13:11:10 +0530

    Update dice_rolling_game.py

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,16 +1,19 @@
 import random
 
 while True:
-  choice=input('roll the dice?(y/n) :').lower()
-    if choice == 'y'
-    die1 = random.randint(1,6)
-    die2 = random.randint(1,6)
-print(f('({die1},{die2})')
-      elif choice == 'n':
-       print('Thanks for playing!')
-       break:
-else:
-  print('Invalid choice!')
+    choice = input('Roll the dice? (y/n): ').lower()
+
+    if choice == 'y':  # Colon (:) was missing
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'({die1}, {die2})')  # Fixed print statement
+
+    elif choice == 'n':  # Corrected elif syntax
+        print('Thanks for playing!')
+        break  # Fixed break placement
+
+    else:
+        print('Invalid choice!')  # Corrected indentation
 
 
 
