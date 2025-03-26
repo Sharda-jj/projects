288b53078f96ad2f1be1425490a37d4859283c49
Sharda-jj <shardajadhav126@gmail.com>
2025-03-26 13:04:54 +0530

    Update dice_rolling_game.py

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,13 +1,17 @@
-import random 
+import random
 
 while True:
-  choice = input('Roll the dice? (y/n): ').lower()
-  if choice == 'y':
-      die1 = random.randint(1, 6)
-      die2 = random.randint(1, 6)
-      print(f'({die1}, {die2})')
-  elif choice == 'n':
-      print('Thanks for playing!')
-      break
-  else:
-      print('Invalid choice!')
+  choice=input('roll the dice?(y/n) :').lower()
+    if choice == 'y'
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+print(f('({die1},{die2})')
+      elif choice == 'n':
+       print('Thanks for playing!')
+       break:
+else:
+  print('Invalid chpice!')
+
+
+
+
